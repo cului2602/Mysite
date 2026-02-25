afd80c73907afad0073d09ee89c705d457233019
tuannguyenlc <tuannguyenlc@gmail.com>
2026-02-25 11:14:07 +0700

    C1_START

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Web phụ kiện điện thoại</title>
+    <link rel="stylesheet" href=".\Css\style.css" />
+  </head>
+  <body>
+    <div class="warpper">
+      <div class="header">
+        <p>Header</p>
+      </div>
+      <div class="menu">
+        <p>Menu</p>
+      </div>
+      <div id ="Main">
+        <div class="sidebar">
+          sidebar
+        </div>
+      </div>
+      <div class="footer">
+        <p>Footer</p>
+      </div>
+    </div>
+  </body>
+</html>
